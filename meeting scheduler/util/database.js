0869defab7ca018e4b0fdee519be14f376f751a6
Dhruv Nagar <112823969+dhruvnagar01@users.noreply.github.com>
2024-01-05 09:47:01 +0530

    Delete meeting scheduler directory

--- meeting scheduler/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize=require('sequelize');
-
-const sequelize=new Sequelize('meetingscheduler','root','rootpassword',({
-    dialect:'mysql',
-    host: 'localhost',
-}))
-
-module.exports=sequelize;
